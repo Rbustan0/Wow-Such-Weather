e97dfe4e8ebc23d9b45ee70fa0ef1249ee9543c2
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-03-28 18:35:57 -0700

    added event listener for search button to appear

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+// Quicker startup
+$(function () {
+
+    // Global Constants/Vars go here:
+    const cityInput = $("#city");
+    const stateInput = $("#state");
+    var searchbtn = $('#search');
+
+
+    // Event Listener for inputs being filled and button appearing for search:
+
+    cityInput.add(stateInput).on('input', function () {
+        // check if both input fields have text in them
+        if (cityInput.val().trim() !== '' && stateInput.val().trim() !== '') {
+            // if both input fields have text, show the search button
+            searchbtn.removeClass('is-hidden');
+        } else {
+            // if either input field is empty, hide the search button
+            searchbtn.addClass('is-hidden');
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
